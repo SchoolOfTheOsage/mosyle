c19101ba9b3f43603b77e71b5bcec98d08dd7600
Nathan C. McGuire <nathanmcguire@users.noreply.github.com>
2023-02-18 22:42:23 -0600

    Update setup.py
    Incorporated setup.py (for humans)

--- setup.py
+++ setup.py
@@ -1,12 +1,115 @@
-from setuptools import find_packages, setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pipenv install twine --dev
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import Command, find_packages, setup
+
+# Package meta-data.
+NAME = "mosyle"
+DESCRIPTION = "Mosyle Python API"
+URL = "https://github.com/SchoolOfTheOsage/mosyle"
+PROJECT_URLS = {}
+EMAIL = "mcguiren@osageschools.org"
+AUTHOR = "Nathan McGuire"
+REQUIRES_PYTHON = ">=3.7.0"
+VERSION = "0.0.4"
+LICENSE = "MIT"
+REQUIRED = ["requests"]
+EXTRAS = {}
+CLASSIFIERS = [
+    # Trove classifiers
+    # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    "Development Status :: 1 - Planning",
+    "Intended Audience :: System Administrators",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python",
+]
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, "readme.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about: dict[str, str] = {}
+if not VERSION:
+    PROJECT_SLUG = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(
+        os.path.join(here, PROJECT_SLUG, "__version__.py"), encoding="UTF-8"
+    ) as file:
+        exec(file.read(), about)
+else:
+    about["__version__"] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(string: str):
+        """Prints things in bold."""
+        print(f"\033[1m{string}\033[0m")
+
+    def run(self):
+        """"""
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system(f"{sys.executable} setup.py sdist bdist_wheel --universal")
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags…")
+        version: str = about["__version__"]
+        os.system(f"git tag v{version}")
+        os.system("git push --tags")
+
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
-    name="mosyle",
-    version="0.0.3",
-    description="Mosyle Manager Python API",
-    author="Nathan McGuire",
-    author_email="mcguiren@osageschools.org",
-    url="https://github.com/SchoolOfTheOsage/mosyle",
-    packages=find_packages(),
-    install_requires=["requests"],
+    name=NAME,
+    version=str(about["__version__"]),
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license=LICENSE,
+    classifiers=CLASSIFIERS,
+    # $ setup.py publish support.
+    cmdclass={
+        "upload": UploadCommand,
+    },
 )
