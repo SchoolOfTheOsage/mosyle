d50d6f543751d79dc8c2da2bdf47a380d2e916e5
Nathan C. McGuire <nathanmcguire@users.noreply.github.com>
2023-03-01 01:43:01 -0600

    Update action.py

--- src/mosyle/action.py
+++ src/mosyle/action.py
@@ -1,2 +1,38 @@
+"""Mosyle Manager Python API - Action"""
+
+
+from .api_client import ApiClient
+from .str_enum import StrEnum
+
+
+class ActionApiResource(StrEnum):
+    """Action API Resource Enumeration"""
+
+    GET = "adminlogs"
+
+
 class Action:
-    pass
+    """Action"""
+
+    def get(
+        self,
+        page: int | None = None,
+        start_date: str | None = None,
+        end_date: str | None = None,
+        id_users: list[str] | None = None,
+    ):
+        """Get Actions"""
+        filter_options: dict[str, object] = {}
+        if start_date is not None:
+            filter_options["start_date"] = start_date
+        if end_date is not None:
+            filter_options["end_date"] = end_date
+        if id_users is not None:
+            filter_options["idusers"] = id_users
+
+        keys: dict[str, object] = {}
+
+        if page is not None:
+            keys["page"] = page
+
+        # return self.post(Endpoint.ACTIONS, keys=keys, filter_options=filter_options)
