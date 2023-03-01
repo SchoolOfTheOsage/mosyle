70e01ee596293a8354b2e0db89628eacc8af107d
Nathan C. McGuire <nathanmcguire@users.noreply.github.com>
2023-03-01 01:42:09 -0600

    Update __init__.py

--- src/mosyle/__init__.py
+++ src/mosyle/__init__.py
@@ -2,13 +2,13 @@
 
 from pkg_resources import DistributionNotFound, get_distribution
 
-from .accounts import *
-from .actions import *
-from .api import *
+from .account import *
+from .action import *
+from .api_client import *
 from .cisco_ise import *
-from .classes import *
-from .devices import *
-from .users import *
+from .device import *
+from .section import *
+from .user import *
 
 try:
     __version__ = get_distribution(__name__).version
