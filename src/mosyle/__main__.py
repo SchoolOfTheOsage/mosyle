7942eb4d81aca45904a150da092e5ac17bc88cd2
Nathan C. McGuire <nathanmcguire@users.noreply.github.com>
2023-03-02 00:49:57 -0600

    Update __main__.py

--- src/mosyle/__main__.py
+++ src/mosyle/__main__.py
@@ -1,16 +1,62 @@
 """Mosyle Manager Python API - Main"""
-# call package mosyle __main__.py
-# python mosyle
-#
-# Call module devices
-# python -m devices
-#
-# Install package pip3 install -e .
+
+import click
+
+import mosyle
+
+from .config import Config
+
+CONFIG = Config()
 
 
-def main():
-    print("Hello World")
+# Main
+@click.group()
+def cli():
+    """Mosyle Manager CLI"""
+    click.echo(f"Mosyle Manager CLI {mosyle.__version__}")
 
 
+# Settings
+@cli.command()
+@click.option(
+    "-t",
+    "--token",
+    prompt="API Token",
+    default=CONFIG.token_obfuscated,
+    help="API Token",
+)
+@click.option(
+    "-u",
+    "--username",
+    prompt="Username",
+    default=CONFIG.username,
+    help="Admin Email",
+)
+@click.option(
+    "-p",
+    "--password",
+    prompt="Password",
+    default=CONFIG.password_obfuscated,
+    help="Admin Password",
+)
+def config(token: str, username: str, password: str):
+    """Mosyle Manager CLI Configuration"""
+    CONFIG.token_obfuscated = token
+    CONFIG.username = username
+    CONFIG.password_obfuscated = password
+
+
+# Account
+
+# Action
+
+# Cisco ISE
+
+# Device
+
+
+# Section
+
+# User
 if __name__ == "__main__":
-    main()
+    cli()
